6239780ed0d7dd76e00cfefb76d8f3681f3a8a8e
IgorBraz <igorbraz81@outlook.com>
2020-03-09 21:34:24 +0000

    Returning to the default folder structure.

--- config/application.rb
+++ config/application.rb
@@ -33,8 +33,5 @@ module TrackerLauncher
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    config.autoload_paths << config.root.join('app', 'web')
-    config.autoload_paths << config.root.join('app', 'web', 'controllers')
   end
 end
